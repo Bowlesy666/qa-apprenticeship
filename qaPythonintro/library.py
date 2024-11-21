a6c95e792482b1b8c022735dbe3db202f61eee57
Bowles, David <David.Bowles@canopius.com>
2024-11-21 10:46:50 +0000

    added book shelf 2 and testing

--- qaPythonintro/library.py
+++ qaPythonintro/library.py
@@ -29,7 +29,7 @@ class BookClass:
         self.publication_year = publication_year
 
     def __str__(self):
-        return f"{self.title} by author: {self.author} published: {self.publication_year}"
+        return f"{self.title}, by author: {self.author}, published by:{self.publisher} in {self.publication_year}"
 
 
 class BookShelfClass:
@@ -43,7 +43,7 @@ class BookShelfClass:
                 self.book_list.append(book)
                 print(f"Added {book.title} to shelf")
             else:
-                print("Oops youve reached book shelf capacity: {self.capacity}")
+                print(f"Oops youve reached book shelf capacity: {self.capacity}")
         else:
             print(f"{book.title} is already on the shelf")
 
@@ -58,6 +58,7 @@ class BookShelfClass:
         for book in self.book_list:
             if book_title == book.title:
                 return f"Book found! {book}"
+        return "Book not found!"
     
     def find_book_by_author(self, book_author):
         books_by_author = []
@@ -66,27 +67,52 @@ class BookShelfClass:
                 books_by_author.append(book)
         
         if len(books_by_author) >= 1:
-            return f"Books found by author are {list(books_by_author)}"
+            return f"Books found by author {book_author} are {', '.join(str(book) for book in books_by_author)}"
         else:
             return "No books found by this author!"
         
-    
-
     def __str__(self):
-        return f"Book shelf capacity: {self.capacity}: {list(self.book_list)}"
+        return f"Book shelf capacity: {self.capacity}: {', '.join(str(book) for book in self.book_list)}"
     
-
-# Example usage:
 book1 = BookClass("1984", "George Orwell", "Secker & Warburg", 1949)
 book2 = BookClass("To Kill a Mockingbird", "Harper Lee", "J.B. Lippincott & Co.", 1960)
 book3 = BookClass("The Great Gatsby", "F. Scott Fitzgerald", "Charles Scribner's Sons", 1925)
 book4 = BookClass("Moby Dick", "Herman Melville", "Harper & Brothers", 1851)
 
+print("\n\n\nBook Shelf 1\n")
 shelf = BookShelfClass(3)
 shelf.add_book(book1)
 shelf.add_book(book2)
 shelf.add_book(book3)
-shelf.add_book(book4)  # This should print a message that the shelf is full
+shelf.add_book(book4)
 
-print(shelf)
+
+book5 = BookClass("The Expanse: Leviathan Wakes", "James S.A. Corey", "Orbit", 2011)
+book6 = BookClass("The Expanse: Caliban's War", "James S.A. Corey", "Orbit", 2012)
+book7 = BookClass("The Hollow Chocolate Bunnies of the Apocalypse", "Robert Rankin", "Gollancz", 2002)
+book8 = BookClass("John Dies at the End", "David Wong", "Thomas Dunne Books", 2007)
+
+shelf2 = BookShelfClass(3)
+shelf2.add_book(book5)
+shelf2.add_book(book6)
+shelf2.add_book(book7)
+shelf2.add_book(book8)
+print("\n\n")
+
+print(shelf2)
+print("\n\n")
+
+shelf2.remove_book(book2)
+print(shelf2)
+print("\n\n")
+
+print(shelf2.find_book_by_title("The Expanse: Leviathan Wakes"))
+print(shelf2.find_book_by_title("Unknown Book"))
+print("\n\n")
+
+print(shelf2.find_book_by_author("James S.A. Corey"))
+print(shelf2.find_book_by_author("Unknown Author"))
+print("\n\n")
+
+print(shelf2)
     

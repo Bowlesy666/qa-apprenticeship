9062225c7b43f9716ecf10ef353222ac90c52cfa
Bowles, David <David.Bowles@canopius.com>
2024-11-22 09:53:57 +0000

    java initial commit

---
+++ java/oneIntro/Lab1.java
@@ -0,0 +1,20 @@
+package oneIntro;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+// In line comments
+/*
+ * Docstrings
+ * are like this!
+ * made to run on diff OS's and CPU's
+ * 
+ * Java is programming language and environment (JDK)
+ * App environment (JRE) runtime environ... JDK has both
+ * Compiled with'javac {filename}.java into a class file then jvm runs the byte code ie faster than python
+ * 
+ * 
+ */
